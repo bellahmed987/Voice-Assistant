ceeafe1f403ea58a971bdeeffe5054a7dc7dce2c
Bilal rana <blalahmed.cs987@gmail.com>
2025-04-29 15:15:15 +0500

    initialize the project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
